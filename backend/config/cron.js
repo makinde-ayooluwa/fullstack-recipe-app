17d64dac7ce78b3574ca9b4c8fe326c19143dd4d
Makinde Ayooluwa <makindeayooluwa604@gmail.com>
2026-07-02 13:26:18 +0100

    another commit

--- backend/config/cron.js
+++ backend/config/cron.js
@@ -1,10 +1,11 @@
 const cron = require("cron");
 const https = require("https");
 const job = new cron.CronJob("*/13 * * * *", () => {
-    https.get(`${process.env.BACKEND_URI_ONLINE}`, () => {
-        console.log(`Cron GET request made successfully`);
+    https.get(`${process.env.BACKEND_URI_ONLINE}`, (res) => {
+        if(res.statusCode === 200) console.log(`Cron GET request made successfully`);
+        else console.log("Error occured while making GET request");
     }).on("error", () => {
-        console.log(`CronJon error occured`);
+        console.log(`CronJob error occured`);
     })
 }).start();
 module.export = job;
