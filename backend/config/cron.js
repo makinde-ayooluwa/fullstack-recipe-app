54f1799d00dc636b93ac5e131600e8b296eb196b
Makinde Ayooluwa <makindeayooluwa604@gmail.com>
2026-07-02 12:48:49 +0100

    first commit

--- backend/config/cron.js
+++ backend/config/cron.js
@@ -1,6 +1,6 @@
 const cron = require("cron");
 const https = require("https");
-const job = new cron.CronJob("*/0 * * * *", () => {
+const job = new cron.CronJob("*/13 * * * *", () => {
     https.get(`${process.env.BACKEND_URI_ONLINE}`, () => {
         console.log(`Cron GET request made successfully`);
     }).on("error", () => {
