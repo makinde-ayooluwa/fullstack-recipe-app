d529ff2b7e82ff37c2ea7c2afb327d9ba1e0e10b
Makinde Ayooluwa <makindeayooluwa604@gmail.com>
2026-07-02 15:31:29 +0100

    new commit

--- backend/config/cron.js
+++ backend/config/cron.js
@@ -1,6 +1,6 @@
 const cron = require("cron");
 const https = require("https");
-const job = new cron.CronJob("*/13 * * * *", () => {
+const job = new cron.CronJob("*/1 * * * *", () => {
     https.get(`${process.env.BACKEND_URI_ONLINE}`, (res) => {
         if(res.statusCode === 200) console.log(`Cron GET request made successfully`);
         else console.log("Error occured while making GET request");
--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -1,5 +1,7 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return <Stack screenOptions={{ headerShown: true }}>
+    <Stack.Screen name="index" options={{ headerStyle: { backgroundColor: "#005712ff" }, headerTintColor: "#fff", headerTitleAlign: "center", title: "Home", headerShown:false }}></Stack.Screen>
+  </Stack>;
 }
