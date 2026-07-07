a82dc0bfaccd38c49bd2fa1748101cfb5ce4c878
Makinde Ayooluwa <makindeayooluwa604@gmail.com>
2026-07-07 12:53:24 +0100

    new commit

--- mobile/app/(auth)/register.jsx
+++ mobile/app/(auth)/register.jsx
@@ -1,14 +1,16 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-
+import { authStyles } from '../../styles/authStyles'
+import { Ionicons, FontAwesome, AntDesign } from '@expo/vector-icons';
 const RegisterScreen = () => {
-  return (
-    <View>
-      <Text>RegisterScreen</Text>
-    </View>
-  )
+    const styles = authStyles['register'];
+    return (
+        <View style={styles.container}>
+            <View style={styles.socialRegister}>
+                <AntDesign name='google' size={30} color={"red"} style={styles.socialIcon} />
+            </View>
+        </View>
+    )
 }
 
 export default RegisterScreen
-
-const styles = StyleSheet.create({})
---
+++ mobile/styles/authStyles.js
@@ -0,0 +1,20 @@
+export const authStyles = {
+    login:{
+        container:{
+            flex: 1,
+        }
+    },
+    register:{
+        container:{
+            flex: 1,
+            justifyContent:"center",
+        },
+        socialRegister:{
+            alignItems:"center",
+            justifyContent:"space-between"
+        },
+        socialIcon:{
+
+        }
+    },
+}
